2620c2f6516a23cc5045a2debb8c72f40d0ef57c
caoliveira88 <camilaoliver1@gmail.com>
2022-11-30 16:10:02 -0300

    :sparkles: Feat - Função para criar tarefa

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ const getLocal = () => JSON.parse(localStorage.getItem('taskList')) || []
 const setLocal = localStge =>
   localStorage.setItem('taskList', JSON.stringify(localStge))
 
+// Criar item  
+const createItem = (nameList, status, descricao, index) => {
+  const item = document.createElement('label')
+  item.classList.add('todo__item')
+  if (status === 'checked') {
+    item.classList.add('ativo')
+  }
+  item.innerHTML = `
+  <div >
+  <div class="todo__top">
+              <input id="teste" type="checkbox" ${status} data-index = '${index}'/>
+              <div>${nameList} </div>
+              <i id="lixo"class="fa-solid fa-trash-can" data-index = '${index}'></i>
+              
+            </div>
+            <div class="desc-list">
+                <textarea  
+                  type="text"
+                  name=""
+                  id="textValue"
+                  cols=""
+                  rows="6"
+                 
+                  maxlength="100"
+                  placeholder="Descricao da tarefa"
+                  >${descricao}</textarea> 
+            </div>
+            <button id="buttonDesc"  data-index = '${index}'>Inserir descricao</button></div>
+  `
+  document.getElementById('todoList').appendChild(item)
+}
+
